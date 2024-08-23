ebc2590b1a73af489b4b74c9f25eb51008543da3
rafatkab <rafat.kabir301@gmail.com>
2024-08-23 16:31:27 -0400

    true init

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Slot } from "expo-router";
 
-const RootLayout = () => {
-  return <Slot />;
+const index = () => {
+  return (
+    <View>
+      <Text>skbidi</Text>
+    </View>
+  );
 };
 
-export default RootLayout;
+export default index;
 
 const styles = StyleSheet.create({});
